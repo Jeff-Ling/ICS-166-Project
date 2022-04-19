d2c2d5a4bf1766cb5c6d0ca4a16b3cc6359a2ac1
Kastorp-S <ieoliasong2013@outlook.com>
2022-04-18 23:15:09 -0700

    update
    Puzzle 1 place holder added;

---
+++ Assets/Scripts/menuControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuControl : MonoBehaviour
+{
+    public static bool isPaused = false; 
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == true)
+            {
+                resumeButton();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void resumeButton()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
