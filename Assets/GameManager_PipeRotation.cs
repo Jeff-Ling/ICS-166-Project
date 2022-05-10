f13edd07a1f8069f19f0f6155bb1139f156d5bfe
Kastorp-S <ieoliasong2013@outlook.com>
2022-05-10 16:35:22 -0700

    update
    Mini game added (NOT FINISH);
    
    Known BUG:
    Pipes won't turn on click;

---
+++ Assets/GameManager_PipeRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_PipeRotation : MonoBehaviour
+{
+    public GameObject PipeHolder;
+    public GameObject[] pipes;
+
+    [SerializeField]
+    int totalPipes = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalPipes = PipeHolder.transform.childCount;
+
+        pipes = new GameObject[totalPipes];
+        
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            pipes[i] = PipeHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/pipeControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeControl : MonoBehaviour
+{
+    public float correctPosition;
+    
+    [SerializeField]
+    bool isCorrected = false;
+
+    float [] rotation = {0, 90, 180, 270};
+
+    private void Start()
+    {
+        int rand = Random.Range(0, rotation.Length);
+        transform.eulerAngles = new Vector3(0, 0, rotation[rand]);
+
+        if (transform.eulerAngles.z == correctPosition)
+        {
+            isCorrected = true;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        transform.Rotate(new Vector3(0, 0, 90));
+
+        if (transform.eulerAngles.z == correctPosition && isCorrected == false)
+        {
+            isCorrected = true;
+        }
+        else if (isCorrected == true)
+        {
+            isCorrected = false;
+        }
+    }
+}
